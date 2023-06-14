c7f1c1fbfb964baed843037e233d298a4643ac15
Jovitech <jovitechgaropaba@gmail.com>
2023-06-14 17:33:48 -0300

    refactoring the code and adding functions

--- main.js
+++ main.js
@@ -1,38 +1,49 @@
+// Variáveis
 const screen1 = document.querySelector('.screen1')
 const screen2 = document.querySelector('.screen2')
-    
 
-const randomNumber = Math.round(Math.random() * 10)
-
-let xAttempts = 1
-
-// Função Callback
-function handleTryClick(event) {
-  event.preventDefault() // "não faça o padrão"
-
-  const inputNumber = document.querySelector('#inputNumber')
-
-  if (Number(inputNumber.value) == randomNumber) {
-    screen1.classList.add('hide')
-    screen2.classList.remove('hide')
-
-    document.querySelector(
-      '.screen2 h2'
-      ).innerText = `acertou em ${xAttempts} tentativas`
-  }
-  
-  inputNumber.value = ""
-  xAttempts++
-}
-
-// Eventos
 const btnTry = document.querySelector("#btnTry")
 const btnReset = document.querySelector("#btnReset")
 
-btnTry.addEventListener('click', handleTryClick)
-btnReset.addEventListener('click', function(){
-    screen1.classList.remove('hide')
-    screen2.classList.add('hide')
+let randomNumber = Math.round(Math.random() * 10)
 
-    xAttempts = 1 
-})
+let xAttempts = 1
+
+// Eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', handleResetEnter)
+
+// Funções
+function handleTryClick(event) {
+    event.preventDefault() // "não faça o padrão"
+
+    const inputNumber = document.querySelector('#inputNumber')
+
+    if (Number(inputNumber.value) == randomNumber) {
+    toggleScreen()
+
+    document.querySelector(
+        '.screen2 h2'
+        ).innerText = `acertou em ${xAttempts} tentativas`
+    }
+    inputNumber.value = ""
+    xAttempts++
+}
+
+function handleResetEnter(e){
+    if(e.key == "Enter" && screen1.classList.contains('hide')) {
+    handleResetClick()
+    }
+
+}
+function handleResetClick(){
+    toggleScreen()
+    xAttempts = 1
+    randomNumber = Math.round(Math.random() * 10)
+}    
+
+function toggleScreen(){
+    screen1.classList.toggle('hide')
+    screen2.classList.toggle('hide')
+}
